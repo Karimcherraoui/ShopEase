75dce0dfc566097d5126714e07fee95ca9f37ffc
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-08 01:54:57 +0100

    Refactor navigation and add Redux store

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/components/Navigation/Stack";
+import store from "./src/store";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Landing" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
